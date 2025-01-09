c068274a70b52c9ecd4274da228d71ad7df999ba
Anastacy Slyusar <ana.milpalm@gmail.com>
2025-01-09 23:02:30 +0100

    step 2

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,13 @@
+import fs from 'fs';
 import { PATH_DB } from '../contacts/contacts.js';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  try {
+    const data = await fs.promises.readFile(PATH_DB, 'utf8');
+
+    return JSON.parse(data);
+  } catch (error) {
+    console.error('Error reading file:', error);
+    throw new Error('Failed to read contacts data');
+  }
+};
