87c69a7cdf3c8271c83fff8436d9c7ae3748e15d
Anastacy Slyusar <ana.milpalm@gmail.com>
2025-01-10 11:04:24 +0100

    Step 3

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,2 +1,23 @@
-export const generateContacts = async (number) => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { PATH_DB } from '../contacts/contacts.js';
+
+export const generateContacts = async (number) => {
+  try {
+    const existingContacts = await readContacts();
+
+    const newContacts = [];
+    for (let i = 0; i < number; i++) {
+      newContacts.push(createFakeContact());
+    }
+
+    const updatedContacts = [...existingContacts, ...newContacts];
+
+    await writeContacts(updatedContacts);
+    console.log(`Successfully added ${number} new contacts.`);
+  } catch (error) {
+    console.error('Error generating contacts:', error);
+  }
+};
 generateContacts(5);
