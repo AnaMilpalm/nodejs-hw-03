ae199ed9345244c82cfa1c18cf67fbcdb3cf0f4f
Anastacy Slyusar <ana.milpalm@gmail.com>
2025-01-10 17:29:33 +0100

    Step 6

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,13 @@
-export const getAllContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
 
-console.log(await getAllContacts);
+export const getAllContacts = async () => {
+  try {
+    const allContacts = await readContacts();
+    console.log('All Contacts:', allContacts);
+    return allContacts;
+  } catch (error) {
+    console.error('Error getting all contacts:', error);
+  }
+};
+
+console.log(await getAllContacts());
