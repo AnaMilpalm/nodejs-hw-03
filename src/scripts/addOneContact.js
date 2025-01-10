9de27f3891a5fda92646b62e8548281027a2e3a3
Anastacy Slyusar <ana.milpalm@gmail.com>
2025-01-10 12:19:35 +0100

    Step 4

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,21 @@
-export const addOneContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+export const addOneContact = async () => {
+  try {
+    const existingContacts = await readContacts();
+
+    const newContact = createFakeContact();
+
+    const updeatedContacts = [...existingContacts, newContact];
+
+    await writeContacts(updeatedContacts);
+
+    console.log('Successfully added a new contact.');
+  } catch (error) {
+    console.error('Error adding new contact:', error);
+  }
+};
 
 addOneContact();
