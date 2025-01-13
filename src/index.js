5b5afd0523745c29c999a8842c750ea4daa551d1
Anastacy Slyusar <ana.milpalm@gmail.com>
2025-01-13 18:00:20 +0100

    middleware

--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
-const message = "Hello world!!!";
-console.log(message);
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+const PORT = 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello world!',
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+app.use(
+  pino({
+    transport: {
+      target: 'pino-pretty',
+    },
+  }),
+);
+
+app.use(cors());
